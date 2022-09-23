38c85cfeb9fa34cff8bbde618e33c6b4a81ac31d
Luke Dulworth <lukedulworth2@gmail.com>
2022-09-22 20:55:02 -0500

    Redid Functions using recursion

--- listFunctions.rkt
+++ listFunctions.rkt
@@ -1,20 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname listFunctions) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
-; Luke Dulworth
-; List Functions Homework
+;Write a function my-list-count that takes a list of size 0, 1, 2 or 3, and produces the length of the list.
 
-; 1. Number of Items in a Small List
-
-; Write a function my-list-count that takes a list of size 0, 1, 2 or 3, and produces the length of the list.
+; Each time it tests if the list is empty
+;   if empty it returns zero
+;   if not empty it adds 1 and trys again 
+;   if empty within the recursion it adds zero and exits (because it does not call itself again)
+; Given: '( '1 '2 '3 '4 '5) Output: 5
+; Recursion Visualization: 1 + (1 + (1 + ( 1 + (1 + 0))))
 
 ; List -> Number
 ; Produces the number of items in the list l
 ; The list is guaranteed to have size 0, 1, 2 or 3
-
-(define (my-list-count l)  (length l))
-
-; 2. Sum of a Small List of Numbers
+(define (my-list-count l)
+  (cond
+    [(empty? l) 0]
+    [else (+ 1 (my-list-count (rest l)))]
+    ))
 
 ; Write a function my-list-sum that consumes a list of size 1, 2 or 3, and produces the sum of the elements of the list.
 
@@ -22,9 +25,18 @@
 ; Produces the sum of the numbers in a list.
 ; The list is guaranteed to have size 1, 2 or 3
 
-(define (my-list-sum lon) (+ (list-ref lon 0) (list-ref lon 1) (list-ref lon 2)))
+(define (my-list-sum lon)
+  (cond
+    [(empty? lon) 0]
+    [else (+ (first lon) (my-list-sum (rest lon)))]
+    ))
 
-; 3. Number of Odd Items in a Small List
+; Each time it tests if the list is empty
+;   if empty it returns zero
+;   if not empty it adds the first item and trys again 
+;   if empty within the recursion it adds zero and exits (because it does not call itself again)
+; Given: '(1 2 3 4 5) Output: 15
+; Recursion Visualization: 1 + (2 + (3 + ( 4 + (5 + 0))))
 
 ; Write a function my-list-odd that consumes a list of size 0, 1, 2 or 3, and produces the number of odd numbers in that list.
 
@@ -32,23 +44,23 @@
 ; Produces the number of odd items in a list lon of numbers
 ; The list is guaranteed to have size 0, 1, 2 or 3.
 
-
-(define (my-list-odd lon) (length (filter odd? lon)))
-
-
-; 4. Number of Odd Items in any list
+(define (my-list-odd lon)
+  (cond
+   [(> (my-list-count lon) 4) "Error: only takes lists of size 0, 1, 2 or 3"]
+   [(empty? lon) 0]
+   [(odd? (first lon)) (+ 1 (my-list-odd (rest lon)))]
+   [else (+ 0 (my-list-odd (rest lon)))]
+   ))
 
 ; Write a function my-list-odd that consumes a list of any size, and produces the number of odd numbers in that list.
 
 ; List of Numbers -> Number
 ; Produces the number of odd items in a list lon of numbers
+(define (my-any-list-odd lon)
+  (cond
+   [(empty? lon) 0]
+   [(odd? (first lon)) (+ 1 (my-any-list-odd (rest lon)))]
+   [else (+ 0 (my-any-list-odd (rest lon)))]
+   ))
 
-(define (my-list-odd2 lon) (length (filter odd? lon)))
 
-
-
-; List -> Boolean
-; outputs #true if l is an empty list, and outputs #false if it is not an empty list. 
-;   l - list which we are testing for being empty or not
-
-;(define (empty? l) (if (eq? l empty) #true #false))
