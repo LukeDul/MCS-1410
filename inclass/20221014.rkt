01b8402468b489d6c56a9616970dd2d047ee13ce
Luke Dulworth <lukedulworth2@gmail.com>
2022-10-14 16:07:08 -0500

    in class notes

---
+++ inclass/20221014.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |20221014|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#|
+Standard deviation
+sd(x) = sqrt(avg (sq list (x - avg( x))))
+
+|#
+
+; List, Number -> List 
+; Takes a list and a number as input and outputs a list containing each element minus x
+
+(define (list_minus lon x)
+  (cond
+    [(empty? lon) empty]
+    [else (cons (- (first lon) x)
+                (list_minus (rest lon) x))]))
