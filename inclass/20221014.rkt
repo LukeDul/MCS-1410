dbebf355252a70b29807b2600e0b56901cd6d904
Luke Dulworth <lukedulworth2@gmail.com>
2022-10-14 16:07:50 -0500

    in class notes

--- inclass/20221014.rkt
+++ inclass/20221014.rkt
@@ -7,11 +7,67 @@ sd(x) = sqrt(avg (sq list (x - avg( x))))
 
 |#
 
+
+
+
+(define-struct WS [ ballx bally xl yl ])
+(define (change-ballpos ws x y) (make-WS x y (WS-xl ws) (WS-yl ws)))
+(define (randomfrac y))
+(define (avg l))
+
+
 ; List, Number -> List 
 ; Takes a list and a number as input and outputs a list containing each element minus x
-
 (define (list_minus lon x)
   (cond
     [(empty? lon) empty]
     [else (cons (- (first lon) x)
                 (list_minus (rest lon) x))]))
+
+
+; List -> List
+(define (sqlist lon)
+  (cond [(empty? lon) empty]
+        [else
+         (cons (* (first lon) (first lon))
+               (sqlist (rest lon)))]))
+
+; List -> Number
+; Given a list of numbers, lon, outputs the standard deviation, a Number,
+; of the elements of lon 
+(define (st_dev lon) (sqrt (avg (sqlist (list_minus lon (avg lon))))))
+
+; Number, Number, Number -> Number
+; Take a convex combination of x and y with weight lambda
+(define (concomb x y l))
+
+
+;WorldState -> Number
+; Give a lambda which is >0 if the sd of xl and yl are both small enough, 0 o/w
+(define (reliable ws (if
+                      (and (< (st_dev (WS-xl ws)) 1)
+                           (< (st_dev (WS-yl ws)) 1))
+                      .01 0)))
+
+(define (render ws)
+  (place-image (circle 10 'solid 'white) (WS-ballx ws) (WS-bally ws)
+               (place-image (square 50 'solid 'black) 575 575
+                            (overlay (square 500 'outline 'black) (square 600 'solid 'green)))))
+
+(define (tock ws)
+  (change-ballpos ws (+ WS-ballx ws0 (* 8 (randokmfrac 100)))
+                  (+ WS-ballx ws0 (* 8 (randokmfrac 100)))))
+                
+
+(define (move ws x y evt)
+  (make-Ws (concomb (avg (WS-xl ws)) (WS-ballx ws) (reliability ws))
+           (concomb (avg (WS-yl ws)) (WS-bally ws) (reliability ws))
+           (adduptotem x (WS-xl ws)) (adduptoten y (WS-yl ws))))
+           
+
+(define (sheep-in-goal? ws))
+
+
+
+
+                  
