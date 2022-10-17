4e27cd24f3d95bc88fd8267751002a918106cc5c
Luke Dulworth <lukedulworth2@gmail.com>
2022-10-17 13:48:46 -0500

    sierpinksi fractals and my-ma p

--- inclass/20221018.rkt
+++ inclass/20221018.rkt
@@ -1,20 +1,41 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |20221018|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
-#| Recursion w/ Images 
+#| Recursion w/ Images and map 
 
 Start with a triangle then start subdividing it into smaller triangles 
 
 each step is called DEPTH-n
-
-
 |#
 
 
-(define (fractal depth width)
+(define (my-sqr n) (* n n))
+
+ 
+(define (my-map f l)
+  (cond
+    [(empty? l) empty]
+    [else (cons (f (first l)) (my-map f (rest l)))]))
+
+
+(define (sierpinksi-triangle depth width)
   (cond
     [(zero? depth) (triangle width 'solid 'white)]
     [else
      (local
-      [(define r (fractal (- depth 1) width))]
+      [(define r (sierpinksi-triangle (- depth 1) width))]
        (above r (beside r r)))]))
+
+
+(define (sierpinksi-carpet size color)
+  (if
+    (<= size 2)
+    (square size 'outline color)
+    (local
+      [(define recurse (sierpinksi-carpet (/ size 3) color))
+      (define center-square (square (/ size 3) 'solid 'white))]
+      (overlay (square size 'outline color)(above (beside recurse recurse recurse)
+                                                    (beside recurse center-square recurse)
+                                                    (beside recurse recurse recurse))))))
+
+     
