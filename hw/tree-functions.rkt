2f51be2004a03e5f6704284592358ea493d5371b
Luke Dulworth <lukedulworth2@gmail.com>
2022-11-28 20:55:57 -0600

    through prob 4

--- hw/tree-functions.rkt
+++ hw/tree-functions.rkt
@@ -90,14 +90,41 @@
 (define (tree-height tree)
   (height-helper tree 0))
 
+
 ; Tree -> Number
 ; returns the height of the given tree
 (define (height-helper tree height)
   (cond [(empty-tree? tree) (- height 1)]
         [(> (height-helper (tree-left tree) height) (height-helper (tree-right tree) height)) (height-helper (tree-left tree) (+ 1 height))]
-        [else (height-helper (tree-right tree) (+ 1 height))]))
-  
+        [else (height-helper (tree-right tree) (+ 1 height))])) 
+
 
 (check-expect (tree-height tree0) 2) 
 (check-expect (tree-height tree1) 9) 
-(check-expect (tree-height tree2) 9)
+(check-expect (tree-height tree2) 9) 
+
+
+; Tree -> List of Lists
+; returns a list of the items in the given tree with their counts sorted alphabetically.  
+(define (tree-sort tree)
+  (reverse (sort-helper tree)))
+
+; Tree -> List of Lists
+; returns a list of the items in the given tree with their counts unsorted.
+(define (sort-helper tree) 
+  (cond [(empty-tree? tree) empty] 
+        [else (append (sort-helper (tree-right tree)) (cons (list (tree-item tree) (tree-count tree)) (sort-helper (tree-left tree))))]))
+
+
+(check-expect (tree-sort tree0) (list (list "a" 1) (list "is" 1) (list "this" 1) (list "tree" 1)))
+(check-expect (tree-sort tree3) (list (list "and" 1) (list "bad" 1) (list "good" 2) (list "the" 4) (list "ugly" 1)))
+
+
+(define (total-count tree)
+  (foldl (lambda (x1 x2) (+ x1 x2)) 0 (total-count-helper tree)))
+
+(define (total-count-helper tree) 
+  (cond [(empty-tree? tree) empty]
+        [else (append (total-count-helper (tree-right tree)) (cons (tree-count tree) (total-count-helper (tree-left tree))))]))
+
+
